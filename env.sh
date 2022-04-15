4d7a6fd913e30f641b7bccfe04873715b86a2887
huloves <17791627852@163.com>
2022-04-15 15:38:04 +0800

    update env.sh

--- env.sh
+++ env.sh
@@ -3,13 +3,13 @@ sudo losetup $losetup_device ./hd.img
 sudo mkfs -q $losetup_device
 
 if [ ! -d "./hdisk" ]; then
-	mkdir ./hdisk
+	sudo mkdir ./hdisk
 fi
 
 sudo mount -o loop ./hd.img ./hdisk
 
 if [ ! -d "./hdisk/boot" ]; then
-	mkdir ./hdisk/boot
-	sudo grub-install --boot-directory=./hdisk/boot/ --force --allow-floppy /dev/loop0
+	sudo mkdir ./hdisk/boot
+	sudo grub-install --boot-directory=./hdisk/boot/ --force --allow-floppy $losetup_device
 fi
 
