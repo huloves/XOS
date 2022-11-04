5c18726fdfa0068e90b01520521e5088fdf0484b
huloves <17791627852@163.com>
2022-11-04 23:06:06 +0800

    add ext2_inode structure

---
+++ include/linux/ext2_fs.h
@@ -0,0 +1,95 @@
+#ifndef _LINUX_EXT2_FS_H
+#define _LINUX_EXT2_FS_H
+
+#include <asm-i386/types.h>
+
+/*
+ * Constants relative to the data blocks
+ */
+#define	EXT2_NDIR_BLOCKS		12
+#define	EXT2_IND_BLOCK			EXT2_NDIR_BLOCKS
+#define	EXT2_DIND_BLOCK			(EXT2_IND_BLOCK + 1)
+#define	EXT2_TIND_BLOCK			(EXT2_DIND_BLOCK + 1)
+#define	EXT2_N_BLOCKS			(EXT2_TIND_BLOCK + 1)
+
+/*
+ * Inode flags
+ */
+#define	EXT2_SECRM_FL				0x00000001 /* Secure deletion */
+#define	EXT2_UNRM_FL				0x00000002 /* Undelete */
+#define	EXT2_COMPR_FL				0x00000004 /* Compress file */
+#define EXT2_SYNC_FL				0x00000008 /* Synchronous updates */
+#define EXT2_IMMUTABLE_FL			0x00000010 /* Immutable file */
+#define EXT2_APPEND_FL				0x00000020 /* writes to file may only append */
+#define EXT2_NODUMP_FL				0x00000040 /* do not dump file */
+#define EXT2_NOATIME_FL				0x00000080 /* do not update atime */
+/* Reserved for compression usage... */
+#define EXT2_DIRTY_FL				0x00000100
+#define EXT2_COMPRBLK_FL			0x00000200 /* One or more compressed clusters */
+#define EXT2_NOCOMP_FL				0x00000400 /* Don't compress */
+#define EXT2_ECOMPR_FL				0x00000800 /* Compression error */
+/* End compression flags --- maybe not all used */	
+#define EXT2_BTREE_FL				0x00001000 /* btree format dir */
+#define EXT2_RESERVED_FL			0x80000000 /* reserved for ext2 lib */
+
+#define EXT2_FL_USER_VISIBLE		0x00001FFF /* User visible flags */
+#define EXT2_FL_USER_MODIFIABLE		0x000000FF /* User modifiable flags */
+
+/*
+ * Structure of an inode on the disk
+ */
+struct ext2_inode {
+	__u16	i_mode;		/* File mode */
+	__u16	i_uid;		/* Low 16 bits of Owner Uid */
+	__u32	i_size;		/* Size in bytes */
+	__u32	i_atime;	/* Access time */
+	__u32	i_ctime;	/* Creation time */
+	__u32	i_mtime;	/* Modification time */
+	__u32	i_dtime;	/* Deletion Time */
+	__u16	i_gid;		/* Low 16 bits of Group Id */
+	__u16	i_links_count;	/* Links count */
+	__u32	i_blocks;	/* Blocks count */
+	__u32	i_flags;	/* File flags */
+	union {
+		struct {
+			__u32  l_i_reserved1;
+		} linux1;
+		struct {
+			__u32  h_i_translator;
+		} hurd1;
+		struct {
+			__u32  m_i_reserved1;
+		} masix1;
+	} osd1;				/* OS dependent 1 */
+	__u32	i_block[EXT2_N_BLOCKS];/* Pointers to blocks */
+	__u32	i_generation;	/* File version (for NFS) */
+	__u32	i_file_acl;	/* File ACL */
+	__u32	i_dir_acl;	/* Directory ACL */
+	__u32	i_faddr;	/* Fragment address */
+	union {
+		struct {
+			__u8	l_i_frag;	/* Fragment number */
+			__u8	l_i_fsize;	/* Fragment size */
+			__u16	i_pad1;
+			__u16	l_i_uid_high;	/* these 2 fields    */
+			__u16	l_i_gid_high;	/* were reserved2[0] */
+			__u32	l_i_reserved2;
+		} linux2;
+		struct {
+			__u8	h_i_frag;	/* Fragment number */
+			__u8	h_i_fsize;	/* Fragment size */
+			__u16	h_i_mode_high;
+			__u16	h_i_uid_high;
+			__u16	h_i_gid_high;
+			__u32	h_i_author;
+		} hurd2;
+		struct {
+			__u8	m_i_frag;	/* Fragment number */
+			__u8	m_i_fsize;	/* Fragment size */
+			__u16	m_pad1;
+			__u32	m_i_reserved2[2];
+		} masix2;
+	} osd2;				/* OS dependent 2 */
+};
+
+#endif
--- include/linux/fs.h
+++ include/linux/fs.h
@@ -4,6 +4,10 @@
 #include <linux/kdev_t.h>
 #include <asm-i386/atomic.h>
 #include <linux/wait.h>
+#include <linux/list.h>
+#include <asm-i386/semaphore.h>
+#include <linux/dcache.h>
+#include <linux/wait.h>
 
 extern void buffer_init(unsigned long);
 
@@ -46,6 +50,102 @@ struct buffer_head {
 	wait_queue_head_t b_wait;
 
 	struct list_head     b_inode_buffers;	/* doubly linked list of inode dirty buffers */
+};
+
+struct inode {
+	struct list_head	i_hash;
+	struct list_head	i_list;
+	struct list_head	i_dentry;   // 链表记录与该文件相联系的所有dentry
+	
+	struct list_head	i_dirty_buffers;
+
+	unsigned long		i_ino;   // 在同一文件系统中每个i节点号都是唯一的
+	atomic_t			i_count;   // 共享计数
+	kdev_t				i_dev;
+	umode_t				i_mode;
+	nlink_t				i_nlink;   // 计数器，用来记住这个文件有多少个链接
+	uid_t				i_uid;
+	gid_t				i_gid;
+	kdev_t				i_rdev;
+	loff_t				i_size;
+	time_t				i_atime;   // 最后一次访问该文件的时间
+	time_t				i_mtime;   // 最后一次修改该文件的时间
+	time_t				i_ctime;   // 最初创建该文件的时间
+	unsigned long		i_blksize;
+	unsigned long		i_blocks;
+	unsigned long		i_version;
+	struct semaphore	i_sem;
+	struct semaphore	i_zombie;
+	struct inode_operations	*i_op;
+	// struct file_operations	*i_fop;	/* former ->i_op->default_file_ops */
+	// struct super_block		*i_sb;
+	wait_queue_head_t		i_wait;
+	// struct file_lock		*i_flock;
+	// struct address_space	*i_mapping;
+	// struct address_space	i_data;	
+	// struct dquot			*i_dquot[MAXQUOTAS];
+	// struct pipe_inode_info	*i_pipe;
+	// struct block_device		*i_bdev;
+
+	unsigned long		i_dnotify_mask; /* Directory notify events */
+	struct dnotify_struct	*i_dnotify; /* for directory notifications */
+
+	unsigned long		i_state;
+
+	unsigned int		i_flags;
+	unsigned char		i_sock;
+
+	atomic_t		i_writecount;
+	unsigned int		i_attr_flags;
+	__u32			i_generation;
+	union {
+		// struct minix_inode_info		minix_i;
+		// struct ext2_inode_info		ext2_i;
+		// struct hpfs_inode_info		hpfs_i;
+		// struct ntfs_inode_info		ntfs_i;
+		// struct msdos_inode_info		msdos_i;
+		// struct umsdos_inode_info	umsdos_i;
+		// struct iso_inode_info		isofs_i;
+		// struct nfs_inode_info		nfs_i;
+		// struct sysv_inode_info		sysv_i;
+		// struct affs_inode_info		affs_i;
+		// struct ufs_inode_info		ufs_i;
+		// struct efs_inode_info		efs_i;
+		// struct romfs_inode_info		romfs_i;
+		// struct shmem_inode_info		shmem_i;
+		// struct coda_inode_info		coda_i;
+		// struct smb_inode_info		smbfs_i;
+		// struct hfs_inode_info		hfs_i;
+		// struct adfs_inode_info		adfs_i;
+		// struct qnx4_inode_info		qnx4_i;
+		// struct bfs_inode_info		bfs_i;
+		// struct udf_inode_info		udf_i;
+		// struct ncp_inode_info		ncpfs_i;
+		// struct proc_inode_info		proc_i;
+		// struct socket			socket_i;
+		// struct usbdev_inode_info        usbdev_i;
+		void				*generic_ip;
+	} u;
+};
+
+struct inode_operations {
+	int (*create) (struct inode *,struct dentry *,int);
+	struct dentry * (*lookup) (struct inode *,struct dentry *);
+	int (*link) (struct dentry *,struct inode *,struct dentry *);
+	int (*unlink) (struct inode *,struct dentry *);
+	int (*symlink) (struct inode *,struct dentry *,const char *);
+	int (*mkdir) (struct inode *,struct dentry *,int);
+	int (*rmdir) (struct inode *,struct dentry *);
+	int (*mknod) (struct inode *,struct dentry *,int,int);
+	int (*rename) (struct inode *, struct dentry *,
+			struct inode *, struct dentry *);
+	int (*readlink) (struct dentry *, char *,int);
+	// int (*follow_link) (struct dentry *, struct nameidata *);
+	void (*truncate) (struct inode *);
+	int (*permission) (struct inode *, int);
+	int (*revalidate) (struct dentry *);
+	// int (*setattr) (struct dentry *, struct iattr *);
+	// int (*getattr) (struct dentry *, struct iattr *);
 };
 
 /* fs/dcache.c */
